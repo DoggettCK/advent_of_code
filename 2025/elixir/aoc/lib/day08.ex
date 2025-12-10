983ee3df9f38a85eba3b82287097193c9eccbbf1
Chris Doggett <doggettc@gmail.com>
2025-12-09 19:55:18 -0600

    Day 8 part 1 cleanup for part 2

--- 2025/elixir/aoc/lib/day08.ex
+++ 2025/elixir/aoc/lib/day08.ex
@@ -1,34 +1,14 @@
 defmodule Day08 do
   @moduledoc false
-  import Common
 
   def part1(args, iterations \\ 10) do
-    points =
-      args
-      |> Stream.with_index(1)
-      |> Map.new(fn {k, v} -> {v, k} end)
+    {graph, shortest_distances} = build_graph_and_shortest_distances(args)
 
-    n = map_size(points)
-
-    graph =
-      points
-      |> Map.keys()
-      |> Enum.reduce(Graph.new(), fn v, g -> Graph.add_vertex(g, v) end)
-
-    shortest_distances =
-      for i <- 1..(n - 1), j <- (i + 1)..n do
-        {dist(Map.get(points, i), Map.get(points, j)), {i, j}}
-      end
-
-    graph =
-      shortest_distances
-      |> Enum.sort()
-      |> Enum.take(iterations)
-      |> Enum.reduce(graph, fn {_distance, {a, b}}, g ->
-        Graph.add_edge(g, a, b)
-      end)
-
-    graph
+    shortest_distances
+    |> Enum.take(iterations)
+    |> Enum.reduce(graph, fn {_distance, {a, b}}, g ->
+      Graph.add_edge(g, a, b)
+    end)
     |> Graph.components()
     |> Enum.group_by(&length/1)
     |> Map.keys()
@@ -38,6 +18,24 @@ defmodule Day08 do
   end
 
   def part2(_args) do
+  end
+
+  defp build_graph_and_shortest_distances(coords) do
+    points =
+      coords
+      |> Stream.with_index(1)
+      |> Map.new(fn {k, v} -> {v, k} end)
+
+    n = map_size(points)
+
+    graph = Enum.reduce(1..n, Graph.new(), fn v, g -> Graph.add_vertex(g, v) end)
+
+    shortest_distances =
+      for i <- 1..(n - 1), j <- (i + 1)..n do
+        {dist(Map.get(points, i), Map.get(points, j)), {i, j}}
+      end
+
+    {graph, Enum.sort(shortest_distances)}
   end
 
   defp dist([x1, y1, z1], [x2, y2, z2]) do
